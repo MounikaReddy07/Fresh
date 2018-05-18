e7cb6f0a3f4aeafd5f4df04861a35a5e49333354
Mounika <mounika.red.pullaiahgari@hpe.com>
2018-05-18 11:41:31 +0530

    Add project files.

---
+++ MVC/Areas/mvcarea/firstController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Areas.mvcarea
+{
+    public class firstController : Controller
+    {
+        // GET: mvcarea/first
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC/Areas/mvcarea/mvcareaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MVC.Areas.mvcarea
+{
+    public class mvcareaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "mvcarea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "mvcarea_default",
+                "mvcarea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
